46fa19f6a786c218d18503861d12862a128c5e8c
0xdeadlee <106419753+0xdeadlee@users.noreply.github.com>
2022-06-02 02:21:02 -0400

    simplifying sznounsauctionhouse (#1)

--- packages/nouns-contracts/contracts/NounsAuctionHouse.sol
+++ packages/nouns-contracts/contracts/NounsAuctionHouse.sol
@@ -188,6 +188,10 @@ contract NounsAuctionHouse is INounsAuctionHouse, PausableUpgradeable, Reentranc
         emit AuctionMinBidIncrementPercentageUpdated(_minBidIncrementPercentage);
     }
 
+    function getDuration() public view virtual returns (uint256) {
+        return duration;
+    }
+
     /**
      * @notice Create an auction.
      * @dev Store the auction details in the `auction` state variable and emit an AuctionCreated event.
@@ -197,7 +201,7 @@ contract NounsAuctionHouse is INounsAuctionHouse, PausableUpgradeable, Reentranc
     function _createAuction() internal {
         try nouns.mint() returns (uint256 nounId) {
             uint256 startTime = block.timestamp;
-            uint256 endTime = startTime + duration;
+            uint256 endTime = startTime + getDuration();
 
             auction = Auction({
                 nounId: nounId,
