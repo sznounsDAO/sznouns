724d1a854d297559cc7cf39ff5a9bbc77628a104
iamminci <0xminci@gmail.com>
2022-06-05 20:39:35 -0400

    REMOVED: nav locale switcher

--- packages/nouns-webapp/src/components/NavBar/index.tsx
+++ packages/nouns-webapp/src/components/NavBar/index.tsx
@@ -21,7 +21,6 @@ import NavBarTreasury from '../NavBarTreasury';
 import NavWallet from '../NavWallet';
 
 import React, { useState } from 'react';
-import NavLocaleSwitcher from '../NavLocaleSwitcher';
 
 const NavBar = () => {
   const activeAccount = useAppSelector(state => state.account.activeAccount);
@@ -133,7 +132,6 @@ const NavBar = () => {
                 buttonStyle={nonWalletButtonStyle}
               />
             </Nav.Link>
-            <NavLocaleSwitcher buttonStyle={nonWalletButtonStyle} />
             <NavWallet address={activeAccount || '0'} buttonStyle={nonWalletButtonStyle} />{' '}
           </Navbar.Collapse>
         </Container>
