15d1ff898d409fad969150b39ed1af0be4d555ab
iamminci <0xminci@gmail.com>
2022-06-06 21:46:52 -0400

    REMOVED: round glasses logic

--- packages/nouns-assets/scripts/encode.ts
+++ packages/nouns-assets/scripts/encode.ts
@@ -31,9 +31,10 @@ const readPngImage = async (path: string): Promise<PngImage> => {
 
 const encode = async () => {
   const encoder = new PNGCollectionEncoder();
+
   const partfolders = ['1-bodies', '2-accessories', '3-heads', '4-glasses'];
   for (const folder of partfolders) {
-    const folderpath = path.join(__dirname, '../flipped', folder);
+    const folderpath = path.join(__dirname, '../images', folder);
     const files = await fs.readdir(folderpath);
     for (const file of files) {
       const image = await readPngImage(path.join(folderpath, file));
