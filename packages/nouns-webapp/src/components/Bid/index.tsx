77a3287f4e5dfa4dfb0e75850358b2ae2d96aa1c
iamminci <0xminci@gmail.com>
2022-06-06 22:58:17 -0400

    REMOVED: fomo nouns stuff

--- packages/nouns-webapp/src/components/Bid/index.tsx
+++ packages/nouns-webapp/src/components/Bid/index.tsx
@@ -236,11 +236,6 @@ const Bid: React.FC<{
   const isDisabled =
     placeBidState.status === 'Mining' || settleAuctionState.status === 'Mining' || !activeAccount;
 
-  const fomoNounsBtnOnClickHandler = () => {
-    // Open Fomo Nouns in a new tab
-    window.open('https://fomonouns.wtf', '_blank')?.focus();
-  };
-
   const isWalletConnected = activeAccount !== undefined;
 
   return (
