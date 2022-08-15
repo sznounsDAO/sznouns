85561dd4dcb3b6b5a02c069c4f1d33f84e63947c
0xgoretex <0xamen@protonmail.com>
2022-08-15 00:33:55 -0400

    descriptor deployment script

--- packages/nouns-contracts/tasks/types/index.ts
+++ packages/nouns-contracts/tasks/types/index.ts
@@ -11,13 +11,13 @@ export type ContractName =
   | 'NFTDescriptor'
   | 'NounsDescriptor'
   // | 'NounsSeeder'
-  | 'SZNounsToken' // replacement for NounsToken
-  | 'SZNounsAuctionHouse' // replacement for NounsAuctionHouse
-  | 'NounsAuctionHouseProxyAdmin'
-  | 'NounsAuctionHouseProxy'
-  | 'NounsDAOExecutor'
-  | 'NounsDAOLogicV1'
-  | 'NounsDAOProxy';
+  // | 'SZNounsToken' // replacement for NounsToken
+  // | 'SZNounsAuctionHouse' // replacement for NounsAuctionHouse
+  // | 'NounsAuctionHouseProxyAdmin'
+  // | 'NounsAuctionHouseProxy'
+  // | 'NounsDAOExecutor'
+  // | 'NounsDAOLogicV1'
+  // | 'NounsDAOProxy';
 
 export interface ContractDeployment {
   args?: (string | number | (() => string))[];
@@ -32,4 +32,9 @@ export interface DeployedContract {
   instance: Contract;
   constructorArguments: (string | number)[];
   libraries: Record<string, string>;
+}
+
+export interface ContractRow {
+  Address: string;
+  'Deployment Hash'?: string;
 }
